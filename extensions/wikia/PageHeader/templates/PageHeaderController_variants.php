7e5a1f5eb572d0e5e2ef2c958bb6e9389eb38c5a
Winston Sung <52590365+WinstonSung@users.noreply.github.com>
2020-09-08 00:22:15 +0800

    Draft of PageHeaderController_variants.php

--- extensions/wikia/PageHeader/templates/PageHeaderController_variants.php
+++ extensions/wikia/PageHeader/templates/PageHeaderController_variants.php
@@ -1,17 +1,18 @@
 <div class="wds-dropdown page-header__languages page-header__variants">
 	<div class="wds-dropdown__toggle">
 		<? // There is intentionally no new line between span and svg to not render additional space between text na svg icon ?>
-		<span data-tracking="variant-dropdown"><?= $languages->currentLangName ?></span><?= DesignSystemHelper::renderSvg(
+		<span data-tracking="variant-dropdown"><?= $variants->currentVariantName ?></span><?= DesignSystemHelper::renderSvg(
 			'wds-icons-dropdown-tiny',
 			'wds-icon wds-icon-tiny wds-dropdown__toggle-chevron'
 		) ?>
 	</div>
 	<div class="wds-dropdown__content wds-is-right-aligned">
 		<ul class="wds-list wds-is-linked">
-			<?php foreach ( $languages->languageList as $key => $val ) : ?>
+			<?php foreach ( $variants as $variant ) : ?>
 				<li>
-					<a href="<?= Sanitizer::encodeAttribute( $val['href'] ); ?>"
-					   data-tracking="top-<?= $key ?>"><?= htmlspecialchars( $val['name'] ); ?></a>
+					<a href="<?= Sanitizer::encodeAttribute( $variant['href'] ); ?>"
+					   id="ca-varlang-<?= $id ?>"
+					   rel="nofollow"><?= htmlspecialchars( $variant['id'] ); ?></a>
 				</li>
 			<?php endforeach ?>
 		</ul>
