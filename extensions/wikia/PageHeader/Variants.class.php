91dab615e33a1eaec8eeeae8592c86e8653b919a
Winston Sung <52590365+WinstonSung@users.noreply.github.com>
2020-09-07 19:19:10 +0800

    Draft of Variants.class.php

--- extensions/wikia/PageHeader/Variants.class.php
+++ extensions/wikia/PageHeader/Variants.class.php
@@ -1,1 +1,18 @@
 
+	/**
+	 * @return array
+	 */
+	private function languageVariants() {
+		$variants = $this->skinTemplate->get( 'content_navigation' )['variants'];
+		if ( !empty( $variants ) ) {
+			return array_map( function ( $variant ) {
+				return Html::element( 'a', [
+					'href' => $variant['href'],
+					'rel' => 'nofollow',
+					'id' => $variant['id'],
+				], $variant['text'] );
+			}, $variants );
+		}
+
+		return [];
+	}
